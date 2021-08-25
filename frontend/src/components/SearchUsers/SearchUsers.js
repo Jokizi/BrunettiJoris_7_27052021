8a8ad7789a0b81b65e97f124518bb4c30818379c
Jokizi <72267511+Jokizi@users.noreply.github.com>
2021-08-25 18:09:15 +0200

    wip SearchUsers

---
+++ frontend/src/components/SearchUsers/SearchUsers.js
@@ -0,0 +1,10 @@
+import Input from "../Input/Input";
+
+const SearchUsers = () => {
+  return (
+    <div>
+      <Input label="chercher un utilisateur" type="text" />
+    </div>
+  );
+};
+export default SearchUsers;
--- frontend/src/container/Home/Home.js
+++ frontend/src/container/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import api from "../../Config/Api";
 import PostMessage from "../../components/PostMessage/PostMessage";
+import SearchUsers from "../../components/SearchUsers/SearchUsers";
 import LikeDislikeMessage from "../../components/LikeMessage/LikeMessage";
 import CommentMessage from "../../components/CommentMessage/CommentMessage";
 import DeleteMessage from "../../components/DeleteMessage/DeleteMessage";
@@ -92,7 +93,9 @@ const Home = ({ myUserId, admin, setAdmin }) => {
       <div style={{ padding: "10px", border: "1px solid black" }}>
         <PostMessage viewMessagesPost={viewMessagesPost} />
       </div>
-
+      <div style={{ padding: "10px", border: "1px solid black" }}>
+        <SearchUsers />
+      </div>
       {allMessages.map((element) => {
         const messageLikeByCurrentUser = element?.Likes?.filter((elt) => groupomaniaUser.id === elt.userId);
 
